2b63f7fa8c5e69743387d13716a7be7a7516e2be
ChrisiPK <ChrisiPK@gmail.com>
2022-05-15 18:32:12 +0000

    add older compiler version for hardhat

--- packages/hardhat-ts/hardhat.config.ts
+++ packages/hardhat-ts/hardhat.config.ts
@@ -91,6 +91,20 @@ export const config: HardhatUserConfig = {
           },
         },
       },
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 250,
+          },
+          outputSelection: {
+            '*': {
+              '*': ['storageLayout'],
+            },
+          },
+        },
+      },
     ],
   },
   mocha: {
