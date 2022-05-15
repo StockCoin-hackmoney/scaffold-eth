1ab39c8ffc8290670c1da2caab45eecf48c2f111
ChrisiPK <ChrisiPK@gmail.com>
2022-05-15 16:34:48 +0000

    add babylon contracts

---
+++ packages/hardhat-ts/contracts/FactoryStockCoin.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "./StockCoin.sol";
+
+// import "./Babylon/interfaces/IBabController.sol";
+
+contract FactoryStockCoin {
+  // constructor(IBabController _controller) {
+  //   require(address(_controller) != address(0), "invalid address");
+  // }
+
+  function deployStockCoin(address synthethicToken) public {
+    // We probabily can try to change this from ERC20 TO ERC1155
+    // address gardenAddress = IBabController.createGarden(
+    //   _reserveAsset,
+    //   _name,
+    //   _symbol,
+    //   _tokenURI,
+    //   _seed,
+    //   _gardenParams,
+    //   _initialContribution,
+    //   _publicGardenStrategistsStewards,
+    //   _profitSharing
+    // );
+
+    address oracle;
+    string memory jobId;
+    address gardenAddress;
+
+    new StockCoin(oracle, jobId, gardenAddress, synthethicToken);
+  }
+}
