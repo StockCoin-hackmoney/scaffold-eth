db676108a261dcd2c29c2459dab5b90f6a4fdcc4
ChrisiPK <ChrisiPK@gmail.com>
2022-05-17 15:00:46 +0000

    fix lockfile, spacing in UMA integration tests

--- packages/hardhat-ts/test/integrationUmaLS.test.js
+++ packages/hardhat-ts/test/integrationUmaLS.test.js
@@ -15,128 +15,128 @@ const NFT_URI = 'https://babylon.mypinata.cloud/ipfs/QmcL826qNckBzEk2P11w4GQrrQF
 const NFT_SEED = '504592746';
 
 describe('Babylon integrations', function () {
-    let owner;
-    let garden;
-    let strategy;
-    let controller;
-    let keeper;
-    let alice;
-    let bob;
+  let owner;
+  let garden;
+  let strategy;
+  let controller;
+  let keeper;
+  let alice;
+  let bob;
 
-    before(async () => {
-        [, keeper, alice, bob] = await ethers.getSigners();
-        controller = await ethers.getContractAt('IBabController', '0xD4a5b5fcB561dAF3aDF86F8477555B92FBa43b5F');
-        owner = await impersonateAddress('0x97FcC2Ae862D03143b393e9fA73A32b563d57A6e');
-        // await controller.connect(owner).addKeeper(keeper.address);
+  before(async () => {
+    [, keeper, alice, bob] = await ethers.getSigners();
+    controller = await ethers.getContractAt('IBabController', '0xD4a5b5fcB561dAF3aDF86F8477555B92FBa43b5F');
+    owner = await impersonateAddress('0x97FcC2Ae862D03143b393e9fA73A32b563d57A6e');
+    // await controller.connect(owner).addKeeper(keeper.address);
 
-        // Creates a garden with custom integrations enabled
-        const contribution = eth(1);
-        await controller.connect(alice).createGarden(
-            WETH,
-            'Fountain',
-            'FTN',
-            NFT_URI,
-            NFT_SEED,
-            [
-                eth(100), // Max Deposit Limit
-                eth(100), // Min Liquidity Asset | ie: Uniswap Volume
-                1, // Deposit Hardlock | 1 second
-                eth(0.1), // Min Contribution
-                ONE_DAY_IN_SECONDS, // Strategy Cooldown Period
-                eth(0.1), // Min Voter Quorum | 10%
-                ONE_DAY_IN_SECONDS * 3, // Min Strategy Duration
-                ONE_DAY_IN_SECONDS * 365, // Max Strategy Duration
-                1, // Min number of voters
-                eth(), // Decay rate of price per share
-                eth(), // Base slippage for price per share
-                1, // Can mint NFT after 1 sec of being a member
-                1 // Whether or not the garden has custom integrations enabled
-            ],
-            contribution,
-            [true, true, true], // publicGardenStrategistsStewards
-            [0, 0, 0], // Profit splits. Use defaults
-            {
-                value: contribution,
-            },
-        );
+    // Creates a garden with custom integrations enabled
+    const contribution = eth(1);
+    await controller.connect(alice).createGarden(
+      WETH,
+      'Fountain',
+      'FTN',
+      NFT_URI,
+      NFT_SEED,
+      [
+        eth(100), // Max Deposit Limit
+        eth(100), // Min Liquidity Asset | ie: Uniswap Volume
+        1, // Deposit Hardlock | 1 second
+        eth(0.1), // Min Contribution
+        ONE_DAY_IN_SECONDS, // Strategy Cooldown Period
+        eth(0.1), // Min Voter Quorum | 10%
+        ONE_DAY_IN_SECONDS * 3, // Min Strategy Duration
+        ONE_DAY_IN_SECONDS * 365, // Max Strategy Duration
+        1, // Min number of voters
+        eth(), // Decay rate of price per share
+        eth(), // Base slippage for price per share
+        1, // Can mint NFT after 1 sec of being a member
+        1 // Whether or not the garden has custom integrations enabled
+      ],
+      contribution,
+      [true, true, true], // publicGardenStrategistsStewards
+      [0, 0, 0], // Profit splits. Use defaults
+      {
+        value: contribution,
+      },
+    );
 
-        const gardens = await controller.getGardens();
-        // console.log(`Garden created at ${gardens[0]}`);
-        garden = await ethers.getContractAt('IGarden', gardens.slice(-1)[0]);
-        // Alternatively you can use mainnet Test WETH garden that has custom integrations enabled
-        // garden = await ethers.getContractAt('IGarden', '0x2c4Beb32f0c80309876F028694B4633509e942D4');
+    const gardens = await controller.getGardens();
+    // console.log(`Garden created at ${gardens[0]}`);
+    garden = await ethers.getContractAt('IGarden', gardens.slice(-1)[0]);
+    // Alternatively you can use mainnet Test WETH garden that has custom integrations enabled
+    // garden = await ethers.getContractAt('IGarden', '0x2c4Beb32f0c80309876F028694B4633509e942D4');
 
+  });
+
+  beforeEach(async () => { });
+
+  afterEach(async () => { });
+
+  it('can deploy a strategy with the UMA LongShortPair Custom integration', async () => {
+
+
+
+    const longShortPair = await ethers.getContractAt("ILongShortPair", "0x94E653AF059550657e839a5DFCCA5a17fD17EFdf");
+
+
+    const collateralToken = await longShortPair.collateralToken();
+
+
+
+
+    // We deploy the custom yearn integration. Change with your own integration when ready
+    const customIntegration = await deploy('CustomIntegrationUmaLongShortPair', {
+      from: alice.address,
+      args: [controller.address],
     });
 
-    beforeEach(async () => { });
+    await garden.connect(alice).addStrategy(
+      'Execute my custom integration',
+      '💎',
+      [
+        eth(10), // maxCapitalRequested: eth(10),
+        eth(0.1), // stake: eth(0.1),
+        ONE_DAY_IN_SECONDS * 30, // strategyDuration: ONE_DAY_IN_SECONDS * 30,
+        eth(0.05), // expectedReturn: eth(0.05),
+        eth(0.1), // maxAllocationPercentage: eth(0.1),
+        eth(0.05), // maxGasFeePercentage: eth(0.05),
+        eth(0.09), // maxTradeSlippagePercentage: eth(0.09),
+      ],
+      [5], // _opTypes
+      [customIntegration.address], // _opIntegrations
+      new ethers.utils.AbiCoder().encode(
+        ['address', 'uint256'],
+        // Long token address? 
+        ['0x94E653AF059550657e839a5DFCCA5a17fD17EFdf', 0] // integration params. We pass USDT vault
+      ), // _opEncodedDatas
+    );
 
-    afterEach(async () => { });
+    console.log("1");
 
-    it('can deploy a strategy with the UMA LongShortPair Custom integration', async () => {
+    const strategies = await garden.getStrategies();
+    customStrategy = await ethers.getContractAt('IStrategy', strategies[0]);
 
-
-
-        const longShortPair = await ethers.getContractAt("ILongShortPair", "0x94E653AF059550657e839a5DFCCA5a17fD17EFdf");
-
-
-        const collateralToken = await longShortPair.collateralToken();
-
-
-
-
-        // We deploy the custom yearn integration. Change with your own integration when ready
-        const customIntegration = await deploy('CustomIntegrationUmaLongShortPair', {
-            from: alice.address,
-            args: [controller.address],
-        });
-
-        await garden.connect(alice).addStrategy(
-            'Execute my custom integration',
-            '💎',
-            [
-                eth(10), // maxCapitalRequested: eth(10),
-                eth(0.1), // stake: eth(0.1),
-                ONE_DAY_IN_SECONDS * 30, // strategyDuration: ONE_DAY_IN_SECONDS * 30,
-                eth(0.05), // expectedReturn: eth(0.05),
-                eth(0.1), // maxAllocationPercentage: eth(0.1),
-                eth(0.05), // maxGasFeePercentage: eth(0.05),
-                eth(0.09), // maxTradeSlippagePercentage: eth(0.09),
-            ],
-            [5], // _opTypes
-            [customIntegration.address], // _opIntegrations
-            new ethers.utils.AbiCoder().encode(
-                ['address', 'uint256'],
-                // Long token address? 
-                ['0x94E653AF059550657e839a5DFCCA5a17fD17EFdf', 0] // integration params. We pass USDT vault
-            ), // _opEncodedDatas
-        );
-
-        console.log("1");
-
-        const strategies = await garden.getStrategies();
-        customStrategy = await ethers.getContractAt('IStrategy', strategies[0]);
-
-        await garden.connect(alice).deposit(eth(1), 0, alice.address, ADDRESS_ZERO, {
-            value: eth(1),
-        });
-
-        console.log("2");
-
-        const balance = await garden.balanceOf(alice.getAddress());
-
-        // Vote Strategy
-        await customStrategy.connect(keeper).resolveVoting([alice.address], [balance], 0);
-
-        console.log("3");
-
-        // Execute strategy
-        await increaseTime(ONE_DAY_IN_SECONDS);
-        await customStrategy.connect(keeper).executeStrategy(eth(1), 0);
-
-        console.log("4");
-
-        // Finalize strategy
-        await increaseTime(ONE_DAY_IN_SECONDS * 30);
-        await customStrategy.connect(keeper).finalizeStrategy(0, '', 0);
+    await garden.connect(alice).deposit(eth(1), 0, alice.address, ADDRESS_ZERO, {
+      value: eth(1),
     });
+
+    console.log("2");
+
+    const balance = await garden.balanceOf(alice.getAddress());
+
+    // Vote Strategy
+    await customStrategy.connect(keeper).resolveVoting([alice.address], [balance], 0);
+
+    console.log("3");
+
+    // Execute strategy
+    await increaseTime(ONE_DAY_IN_SECONDS);
+    await customStrategy.connect(keeper).executeStrategy(eth(1), 0);
+
+    console.log("4");
+
+    // Finalize strategy
+    await increaseTime(ONE_DAY_IN_SECONDS * 30);
+    await customStrategy.connect(keeper).finalizeStrategy(0, '', 0);
+  });
 });
